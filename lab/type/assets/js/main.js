6f514b2c6badf90fa9913e03b68030c8f6532d74
Maria Magnani <magnm834@newschool.edu>
2017-03-30 10:44:41 -0400

    added js 2

---
+++ lab/type/assets/js/main.js
@@ -0,0 +1,111 @@
+$(window).keypress(function(e) {
+	console.log (e.which);
+	if (e.which == 97) {
+	$("body").append($(".a").clone().removeClass(".a"));
+	}
+});
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 66) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 67) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 68) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 69) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 70) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 71) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 72) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 73) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 74) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 75) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 76) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 77) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 78) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 79) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
+
+// $(window).keypress(function(e) {
+// 	console.log (e.which);
+// 	if (e.which == 80) {
+// 	$("body").append($(".a").clone().removeClass(".a"));
+// 	}
+// });
