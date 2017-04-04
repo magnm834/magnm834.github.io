cbb5b5ab698dbdbd48b50aa0f262e342aa850460
Maria Magnani <magnm834@newschool.edu>
2017-04-04 11:02:05 -0400

    started adding layers

--- lab/type/assets/js/main.js
+++ lab/type/assets/js/main.js
@@ -1,212 +1,227 @@
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 97) {
-	$("body").append($(".a").clone().removeClass("a"));
-	}
-});
+var lastLetter = false;
+
 
 $(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 98) {
-	$("body").append($(".b").clone().removeClass("b"));
-	}
-});
+console.log(e.which);
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 99) {
-	$("body").append($(".c").clone().removeClass("c"));
-	}
-});
+	if (e.which == 97 || e.which == 65) {
+		if (lastLetter == 97) {
+			$(".stage:last-child .a_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".a").clone().removeClass("a"));
+			lastLetter = 97;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 100) {
-	$("body").append($(".d").clone().removeClass("d"));
-	}
-});
+	if (e.which == 98 || e.which == 66) {
+		if (lastLetter == 98) {
+			$(".stage:last-child .b_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".b").clone().removeClass("b"));
+			lastLetter = 98;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 101) {
-	$("body").append($(".e").clone().removeClass("e"));
-	}
-});
+	if (e.which == 99 || e.which == 67) {
+		if (lastLetter == 99) {
+			$(".stage:last-child .c_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".c").clone().removeClass("c"));
+			lastLetter = 99;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 102) {
-	$("body").append($(".f").clone().removeClass("f"));
-	}
-});
+	if (e.which == 100 || e.which == 68) {
+		if (lastLetter == 100) {
+			$(".stage:last-child .d_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".d").clone().removeClass("d"));
+			lastLetter = 100;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 103) {
-	$("body").append($(".g").clone().removeClass("g"));
-	}
-});
+	if (e.which == 101 || e.which == 69) {
+		if (lastLetter == 101) {
+			$(".stage:last-child .e_two").css("opacity",1);
+		} 
+		if (lastLetter == 101) {
+			$(".stage:last-child .e_three").css("opacity",1);
+		}
+		if (lastLetter == 101) {
+			$(".stage:last-child .e_four").css("opacity",1);
+			lastLetter = false;
+		}else {
+			$(".stage").append($(".e").clone().removeClass("e"));
+			lastLetter = 101;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 104) {
-	$("body").append($(".h").clone().removeClass("h"));
-	}
-});
+	if (e.which == 102 || e.which == 70) {
+		if (lastLetter == 102) {
+			$(".stage:last-child .f_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".f").clone().removeClass("f"));
+			lastLetter = 102;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 105) {
-	$("body").append($(".i").clone().removeClass("i"));
+	if (e.which == 103 || e.which == 71) {
+		if (lastLetter == 103) {
+			$(".stage:last-child .g_two").css("opacity",1);
+		} 
+		if (lastLetter == 103) {
+			$(".stage:last-child .g_three").css("opacity",1);
+		}
+		if (lastLetter == 103) {
+			$(".stage:last-child .g_four").css("opacity",1);
+			lastLetter = false;
+		}else {
+			$(".stage").append($(".g").clone().removeClass("g"));
+			lastLetter = 103;
+		}
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 106) {
-	$("body").append($(".j").clone().removeClass("j"));
+	if (e.which == 104 || e.which == 72) {
+		if (lastLetter == 104) {
+			$(".stage:last-child .h_two").css("opacity",1);
+		} 
+		if (lastLetter == 104) {
+			$(".stage:last-child .h_three").css("opacity",1);
+		}
+		if (lastLetter == 104) {
+			$(".stage:last-child .h_four").css("opacity",1);
+			lastLetter = false;
+		}else {
+			$(".stage").append($(".h").clone().removeClass("h"));
+			lastLetter = 104;
+		}
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 107) {
-	$("body").append($(".k").clone().removeClass("k"));
+	if (e.which == 105 || e.which == 72) {
+	$(".stage").append($(".i").clone().removeClass("i"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 108) {
-	$("body").append($(".l").clone().removeClass("l"));
+	if (e.which == 106 || e.which == 73) {
+		if (lastLetter == 106) {
+			$(".stage:last-child .j_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".j").clone().removeClass("j"));
+			lastLetter = 106;
+		}
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 109) {
-	$("body").append($(".m").clone().removeClass("m"));
-	}
-});
+	if (e.which == 107 || e.which == 74) {
+		if (lastLetter == 107) {
+			$(".stage:last-child .k_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".k").clone().removeClass("k"));
+			lastLetter = 107;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
-	if (e.which == 110) {
-	$("body").append($(".n").clone().removeClass("n"));
-	}
-});
+	if (e.which == 108 || e.which == 75) {
+		if (lastLetter == 108) {
+			$(".stage:last-child .l_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".l").clone().removeClass("l"));
+			lastLetter = 108;
+		}
+	} 
 
-$(window).keypress(function(e) {
-	console.log(e.which);
+	if (e.which == 109 || e.which == 76) {
+		if (lastLetter == 109) {
+			$(".stage:last-child .m_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".m").clone().removeClass("m"));
+			lastLetter = 109;
+		}
+	} 
+
+	if (e.which == 110 || e.which == 77) {
+		if (lastLetter == 110) {
+			$(".stage:last-child .n_two").css("opacity",1);
+			lastLetter = false;
+		} else {
+			$(".stage").append($(".n").clone().removeClass("n"));
+			lastLetter = 110;
+		}
+	} 
+
 	if (e.which == 111) {
-	$("body").append($(".o").clone().removeClass("o"));
+	$(".stage").append($(".o").clone().removeClass("o"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 112) {
-	$("body").append($(".p").clone().removeClass("p"));
+	$(".stage").append($(".p").clone().removeClass("p"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 113) {
-	$("body").append($(".q").clone().removeClass("q"));
+	$(".stage").append($(".q").clone().removeClass("q"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 114) {
-	$("body").append($(".r").clone().removeClass("r"));
+	$(".stage").append($(".r").clone().removeClass("r"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 115) {
-	$("body").append($(".s").clone().removeClass("s"));
+	$(".stage").append($(".s").clone().removeClass("s"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 116) {
-	$("body").append($(".t").clone().removeClass("t"));
+	$(".stage").append($(".t").clone().removeClass("t"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 117) {
-	$("body").append($(".u").clone().removeClass("u"));
+	$(".stage").append($(".u").clone().removeClass("u"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 118) {
-	$("body").append($(".v").clone().removeClass("v"));
+	$(".stage").append($(".v").clone().removeClass("v"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 119) {
-	$("body").append($(".w").clone().removeClass("w"));
+	$(".stage").append($(".w").clone().removeClass("w"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 120) {
-	$("body").append($(".x").clone().removeClass("x"));
+	$(".stage").append($(".x").clone().removeClass("x"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 121) {
-	$("body").append($(".y").clone().removeClass("y"));
+	$(".stage").append($(".y").clone().removeClass("y"));
 	}
-});
 
-$(window).keypress(function(e) {
-	console.log(e.which);
 	if (e.which == 122) {
-	$("body").append($(".z").clone().removeClass("z"));
+	$(".stage").append($(".z").clone().removeClass("z"));
 	}
-});
 
-$(window).keypress(function(e) {
 	if (e.which == 46) {
-	$("body").append($(".period").clone().removeClass("period"));
+	$(".stage").append($(".period").clone().removeClass("period"));
 	}
-});
 
-$(window).keypress(function(e) {
 	if (e.which == 32) {
-	$("body").append($(".space").clone().removeClass("space"));
+	$(".stage").append($(".space").clone().removeClass("space"));
 	}
-});
 
-$(window).keypress(function(e) {
 	if (e.which == 13) {
-	$("body").append($(".enter").clone().removeClass("enter"));
+	$(".stage").append($(".enter").clone().removeClass("enter"));
 	}
-});
 
-$(window).keypress(function(e) {
 	if (e.which == 44) {
-	$("body").append($(".comma").clone().removeClass("comma"));
+	$(".stage").append($(".comma").clone().removeClass("comma"));
 	}
-});
 
-$(window).keypress(function(e) {
 	if (e.which == 63) {
-	$("body").append($(".question").clone().removeClass("question"));
+	$(".stage").append($(".question").clone().removeClass("question"));
 	}
 });
 
