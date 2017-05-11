a79dd324af9cb5f49926491d86245fac6d5d2993
Maria Magnani <magnm834@newschool.edu>
2017-05-11 09:20:01 -0400

    add chance to repo

---
+++ lab/chance/assets/js/chance.js
@@ -0,0 +1,328 @@
+
+
+
+
+var concreteNouns 				= ["HEAD",
+								   "HEART",
+								   "LIP",
+								   "HAND",
+								   "MOUTH",
+								   "TONGUE",
+								   "EYE",
+								   "FIST",
+								   "CANDLE",
+								   "LIGHT",
+								   "FIRE",
+								   "BODY"];
+
+var concreteNounImgs 			= ["assets/img/concrete_nouns/head.jpg",
+								   "assets/img/concrete_nouns/heart.gif",
+								   "assets/img/concrete_nouns/lip.jpg",
+								   "assets/img/concrete_nouns/hand.jpg",
+								   "assets/img/concrete_nouns/mouth.gif",
+								   "assets/img/concrete_nouns/tongue.jpg",
+								   "assets/img/concrete_nouns/eye.jpg",
+								   "assets/img/concrete_nouns/fist.jpg",
+								   "assets/img/concrete_nouns/candle.jpg",
+								   "assets/img/concrete_nouns/light.jpg",
+								   "assets/img/concrete_nouns/fire.gif",
+								   "assets/img/concrete_nouns/body.gif"];
+	
+
+
+
+
+var abstractNouns 				= ["TRUST",
+								   "LUST",
+								   "ANGER",
+								   "LOVE",
+								   "PASSION",
+								   "FEAR",
+								   "COMPASSION",
+								   "PLEASURE",
+								   "BRILLIANCE",
+								   "SLAVERY",
+								   "SOLITUDE",
+								   "BALANCE"]
+			  
+var abstractNounsImgs 			= ["assets/img/abstract_nouns/trust.gif",
+								   "assets/img/abstract_nouns/lust.jpg",
+								   "assets/img/abstract_nouns/anger.gif",
+								   "assets/img/abstract_nouns/love.png",
+								   "assets/img/abstract_nouns/passion.jpg",
+								   "assets/img/abstract_nouns/fear.gif",
+								   "assets/img/abstract_nouns/compassion.jpg",
+								   "assets/img/abstract_nouns/pleasure.gif",
+								   "assets/img/abstract_nouns/brilliance.gif",
+								   "assets/img/abstract_nouns/slavery.gif",
+								   "assets/img/abstract_nouns/solitude.jpg",
+								   "assets/img/abstract_nouns/balance.gif"];
+
+
+
+
+
+var transitiveVerbs 			= ["HOPE",
+								   "FIGHT",
+								   "TAKE",
+								   "SHOW",
+								   "THROW",
+								   "STOP",
+								   "CHANGE",
+								   "WRITE",
+								   "FIND",
+								   "DESTROY",
+								   "SCREAM",
+								   "BREAK"]
+
+var transitiveVerbsImgs 		= ["assets/img/transitive_verbs/hope.jpg",
+								   "assets/img/transitive_verbs/fight.gif",
+								   "assets/img/transitive_verbs/take.jpg",
+								   "assets/img/transitive_verbs/show.gif",
+								   "assets/img/transitive_verbs/throw.jpg",
+								   "assets/img/transitive_verbs/stop.jpg",
+								   "assets/img/transitive_verbs/change.gif",
+								   "assets/img/transitive_verbs/write.gif",
+								   "assets/img/transitive_verbs/find.jpg",
+								   "assets/img/transitive_verbs/destroy.gif",
+								   "assets/img/transitive_verbs/scream.gif",
+								   "assets/img/transitive_verbs/break.gif"];								
+
+
+
+
+var intransitiveVerbs 			= ["PUSH",
+								   "TOUCH",
+								   "SPEAK",
+								   "LICK",
+								   "SEE",
+								   "HEAR",
+								   "WHISPER",
+								   "HEAL",
+								   "PRAY",
+								   "CREATE",
+								   "DISSOLVE",
+								   "DISSAPEAR"]
+
+var intransitiveVerbsImgs 		= ["assets/img/intransitive_verbs/push.gif",
+								   "assets/img/intransitive_verbs/touch.gif",
+								   "assets/img/intransitive_verbs/speak.gif",
+								   "assets/img/intransitive_verbs/lick.gif",
+								   "assets/img/intransitive_verbs/see.gif",
+								   "assets/img/intransitive_verbs/hear.jpg",
+								   "assets/img/intransitive_verbs/whisper.jpg",
+								   "assets/img/intransitive_verbs/heal.jpg",
+								   "assets/img/intransitive_verbs/pray.gif",
+								   "assets/img/intransitive_verbs/create.gif",
+								   "assets/img/intransitive_verbs/dissolve.gif",
+								   "assets/img/intransitive_verbs/dissapear.gif"];
+
+
+
+var adjectives 					= ["LOYAL",
+								   "BEAUTIFUL",
+								   "SOFT",
+								   "DARK",
+								   "CORAGEOUS",
+								   "AFFECTIONATE",
+								   "PHILOSOPHICAL",
+								   "STRONG",
+								   "DELICATE",
+								   "SMOOTH",
+								   "BOLD",
+								   "EXTRAVAGANT"]
+
+var adjectivesImgs 				= ["assets/img/adjectives/loyal.jpg",
+								   "assets/img/adjectives/beauty.jpg",
+								   "assets/img/adjectives/soft.gif",
+								   "assets/img/adjectives/dark.jpg",
+								   "assets/img/adjectives/corageous.gif",
+								   "assets/img/adjectives/affectionate.gif",
+								   "assets/img/adjectives/philosophical.jpg",
+								   "assets/img/adjectives/strong.jpg",
+								   "assets/img/adjectives/delicate.jpg",
+								   "assets/img/adjectives/smooth.jpg",
+								   "assets/img/adjectives/bold.gif",
+								   "assets/img/adjectives/extravagant.gif"];								   
+
+
+
+
+var adverbs 					= ["HOPELESSLY",
+								   "SILENTLY",
+								   "MADLY",
+								   "SADLY",
+								   "WHOLLY",
+								   "SLOWLY",
+								   "VIOLENTLY",
+								   "FREELY",
+								   "EMPTILY",
+								   "VICIOUSLY",
+								   "RIGIDLY",
+								   "WILDLY"]
+
+var adverbsImgs 				= ["assets/img/adverbs/hopelessly.jpg",
+								   "assets/img/adverbs/silently.gif",
+								   "assets/img/adverbs/madly.gif",
+								   "assets/img/adverbs/sadly.gif",
+								   "assets/img/adverbs/wholly.gif",
+								   "assets/img/adverbs/slowly.gif",
+								   "assets/img/adverbs/violently.gif",
+								   "assets/img/adverbs/freely.gif",
+								   "assets/img/adverbs/empty.jpg",
+								   "assets/img/adverbs/viciously.gif",
+								   "assets/img/adverbs/rididly.jpg",
+								   "assets/img/adverbs/wildly.gif"]
+
+
+var composition 				= ["chance_2.html",
+								   "chance_3.html",
+								   "chance_4.html",
+								   "chance_5.html",
+								   "chance_6.html",
+								   "chance_7.html",
+								   "chance_8.html",
+								   "chance_9.html",
+								   "chance_10.html",
+								   "chance_11.html",
+								   "chance_12.html",
+								   "chance_13.html"];
+
+var compKey 					= ["secondPhrase",
+								   "thirdPhrase",
+								   "fourthPhrase",
+								   "fifthPhrase",
+								   "sixthPhrase",
+								   "seventhPhrase",
+								   "eighthPhrase",
+								   "ninthPhrase",
+								   "tenthPhrase",
+								   "eleventhPhrase",
+								   "twelfthPhrase",
+								   "thirteenthPhrase"];
+
+
+
+
+function getImages() {
+
+
+				var randKeyCnouns = Math.floor(Math.random()*concreteNouns.length);
+			$('.one').html("<div class='word'>" + concreteNouns[randKeyCnouns] +  "</div>");
+			$('.one').append("<img src='" + concreteNounImgs[randKeyCnouns] + "'>");
+		
+				var randKeyCnounsSecond = Math.floor(Math.random()*concreteNouns.length);
+			$('.one_second').html("<div class='word'>" + concreteNouns[randKeyCnounsSecond] +  "</div>");
+			$('.one_second').append("<img src='" + concreteNounImgs[randKeyCnounsSecond] + "'>");
+		
+				var randKeyAnouns = Math.floor(Math.random()*abstractNouns.length);
+			$('.two').html("<div class='word'>" + abstractNouns[randKeyAnouns] +  "</div>");
+			$('.two').append("<img src='" + abstractNounsImgs[randKeyAnouns] + "'>");
+		
+					var randKeyAnouns = Math.floor(Math.random()*abstractNouns.length);
+			$('.two_second').html("<div class='word'>" + abstractNouns[randKeyAnouns] +  "</div>");
+			$('.two_second').append("<img src='" + abstractNounsImgs[randKeyAnouns] + "'>");
+		
+					var randKeyAnouns = Math.floor(Math.random()*abstractNouns.length);
+			$('.two_third').html("<div class='word'>" + abstractNouns[randKeyAnouns] +  "</div>");
+			$('.two_third').append("<img src='" + abstractNounsImgs[randKeyAnouns] + "'>");
+		
+				var randKeytransitiveVerbs = Math.floor(Math.random()*transitiveVerbs.length);
+			$('.three').html("<div class='word'>" + transitiveVerbs[randKeytransitiveVerbs] +  "</div>");
+			$('.three').append("<img src='" + transitiveVerbsImgs[randKeytransitiveVerbs] + "'>");
+		
+					var randKeyintransitiveVerbs = Math.floor(Math.random()*intransitiveVerbs.length);
+			$('.four').html("<div class='word'>" + intransitiveVerbs[randKeyintransitiveVerbs] +  "</div>");
+			$('.four').append("<img src='" + intransitiveVerbsImgs[randKeyintransitiveVerbs] + "'>");
+		
+				var randKeyadjectives = Math.floor(Math.random()*adjectives.length);
+			$('.five').html("<div class='word'>" + adjectives[randKeyadjectives] +  "</div>");
+			$('.five').append("<img src='" + adjectivesImgs[randKeyadjectives] + "'>");
+		
+					var randKeyadjectives = Math.floor(Math.random()*adjectives.length);
+			$('.five_second').html("<div class='word'>" + adjectives[randKeyadjectives] +  "</div>");
+			$('.five_second').append("<img src='" + adjectivesImgs[randKeyadjectives] + "'>");
+		
+					var randKeyadjectives = Math.floor(Math.random()*adjectives.length);
+			$('.five_third').html("<div class='word'>" + adjectives[randKeyadjectives] +  "</div>");
+			$('.five_third').append("<img src='" + adjectivesImgs[randKeyadjectives] + "'>");
+		
+					var randKeyadjectives = Math.floor(Math.random()*adjectives.length);
+			$('.five_fourth').html("<div class='word'>" + adjectives[randKeyadjectives] +  "</div>");
+			$('.five_fourth').append("<img src='" + adjectivesImgs[randKeyadjectives] + "'>");
+		
+					var randKeyadverbs = Math.floor(Math.random()*adverbs.length);
+			$('.six').html("<div class='word'>" + adverbs[randKeyadverbs] +  "</div>");
+			$('.six').append("<img src='" + adverbsImgs[randKeyadverbs] + "'>");
+		
+
+
+}
+
+
+		$('.refresh').click(function() {
+		
+			getImages();
+		});
+
+		getImages();
+		
+		
+		$('.next').click(function() {
+		
+			var hash = window.location.hash;
+		
+			var randKeyComposition = Math.floor(Math.random()*composition.length);
+			if (hash == '') {
+				var url = composition[randKeyComposition] + '#' + compKey[randKeyComposition];
+			} else {
+				var url = composition[randKeyComposition] + hash + ',' + compKey[randKeyComposition];	
+			}
+			
+			window.location = url; 
+		
+		
+		});
+
+
+		$('.print_me').click(function(e) {
+
+		e.preventDefault();
+			var hash = window.location.hash;
+	
+				var url =  "entire_poem.html" + hash;
+	
+			
+			window.location = url; 
+		
+		
+		});
+
+	
+
+	if ($(".entirepoemmarker").length) {
+					$(".verse").hide();
+
+		console.log("you are on the print screen..");
+		var hash = window.location.hash.substr(1);
+		console.log(hash);
+		var classes = hash.split(",");
+		for (var i = 0; i < classes.length; i++) {
+			console.log(classes[i]);
+		$("." + classes[i]).show();
+		}
+	}
+
+
+
+
+// var firstPhrase = "The " + adjectives[randAdject] + " " + randCnouns + " " + randAdverb + " " + randTverbs + "s the " + randCnouns + ".";
+
+// $("body").append("<img src='assets/img/" + randCnoun_to_use + "'>");
+
+
+// console.log(firstPhrase);
+
+// $(document).ready(function() {
+// 	$('.concrete_nouns').append(concreteNouns[randCnouns]);
+// });
